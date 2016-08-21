33fd2285b94109f5eff9bb34e70387bdfdd5fb8e
SeungHwan Kim <Super@SeungHwanui-MacBook-Pro.local>
2016-08-21 19:42:05 +0900

    AppInitializer Commit

---
+++ src/main/java/com/instagram/config/AppInitializer.java
@@ -0,0 +1,39 @@
+package com.instagram.config;
+
+import org.apache.tiles.web.util.TilesDecorationFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by sshkim on 2016. 8. 13..
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+            RootConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+            WebConfig.class
+        };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[]{ characterEncodingFilter, new TilesDecorationFilter()};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
