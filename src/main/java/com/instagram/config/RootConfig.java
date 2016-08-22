c9aa8dbe4e6b0d0b388299467fba5e0d6017fb72
SeungHwan Kim <Super@SeungHwanui-MacBook-Pro.local>
2016-08-22 23:18:11 +0900

    spring4 + hibernate setting - 1

--- src/main/java/com/instagram/config/RootConfig.java
+++ src/main/java/com/instagram/config/RootConfig.java
@@ -1,7 +1,13 @@
 package com.instagram.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /**
  * Created by sshkim on 2016. 8. 18..
  */
+
+@Configuration
+@EnableTransactionManagement
 public class RootConfig {
 }
